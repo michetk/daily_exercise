80937ea8a4f59f1e650788df1d65a7246a457597
Michel Miguel <michetkmiguel@gmail.com>
2022-04-23 14:06:38 -0300

    Config Eslint, Prettier, Husky, .editorconfig and styled-components

--- 001/next.config.js
+++ 001/next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-<<<<<<< HEAD
   compiler: {
     styledComponents: true,
   },
-=======
->>>>>>> 6860e129d925887e5da09762246bc2b34cbd7cdb
 };
 
 module.exports = nextConfig;
