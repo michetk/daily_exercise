661de3277fc699ded2f9859147420711e3f7d600
Michel Miguel <michetkmiguel@gmail.com>
2022-05-01 18:48:33 -0300

    Exercise Simple Dashboard with Next.js

--- 004/src/components/sidebar/styled.ts
+++ 004/src/components/sidebar/styled.ts
@@ -3,6 +3,6 @@ import styled from 'styled-components'
 export const SidebarContainer = styled.aside<{ isExpanded: boolean }>`
   display: flex;
   justify-content: center;
-  width: ${isExpanded => (isExpanded ? '20vw' : '0vw')};
+  width: ${({ isExpanded }) => (isExpanded ? '20vw' : '0vw')};
   overflow: hidden;
 `
